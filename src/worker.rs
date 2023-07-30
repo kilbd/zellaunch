6715e48e7b3881bacd0ab579da07b1ea95d20c3d
Drew Kilb <drew.kilb@duke.edu>
2023-07-30 17:29:39 -0400

    feat: Finds files with shebang instruction
    I don't seem to be able to discern whether a file is executable,
    but I can always attempt to read the file and see if the first
    line has a shebang which is used to execute the file.

--- src/worker.rs
+++ src/worker.rs
@@ -1,4 +1,6 @@
 use serde::{Deserialize, Serialize};
+use std::fs;
+use std::io::{BufRead, BufReader};
 use zellij_tile::{shim::post_message_to_plugin, ZellijWorker};
 
 #[derive(Default, Serialize, Deserialize)]
@@ -9,7 +11,21 @@ impl<'de> ZellijWorker<'de> for TaskDiscoveryWorker {
         let mut files: Vec<String> = vec![];
         for f in ignore::Walk::new("/host") {
             if let Ok(entry) = f {
-                files.push(entry.path().to_str().unwrap().to_string());
+                if let Some(ft) = entry.file_type() {
+                    if ft.is_dir() {
+                        continue;
+                    }
+                    let file = match fs::File::open(entry.path()) {
+                        Ok(file) => file,
+                        Err(_) => continue,
+                    };
+                    // Check first line of file for shebang
+                    let mut lines = BufReader::new(file).lines();
+                    if lines.next().unwrap().unwrap().starts_with("#!") {
+                        eprintln!("found shebang: {:?}", entry.path());
+                        files.push(entry.path().to_str().unwrap()[5..].to_string());
+                    }
+                }
             }
         }
         eprintln!("files: {:?}", files);
