cad0c4f7647e8b8baaa456f96e3d07ed0a4d9715
Drew Kilb <drew.kilb@duke.edu>
2023-07-30 21:49:02 -0400

    feat: Parse `package.json` to get npm tasks

--- src/worker.rs
+++ src/worker.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader};
+use std::path::Path;
 use std::{fmt, fmt::Display, fs};
 use zellij_tile::{shim::post_message_to_plugin, ZellijWorker};
 
@@ -21,9 +23,16 @@ pub struct TaskOption {
 
 impl<'de> ZellijWorker<'de> for TaskDiscoveryWorker {
     fn on_message(&mut self, _message: String, _payload: String) {
-        let mut files: Vec<TaskOption> = vec![];
+        let mut options: Vec<TaskOption> = vec![];
         for f in ignore::Walk::new("/host") {
             if let Ok(entry) = f {
+                if entry
+                    .path()
+                    .file_name()
+                    .is_some_and(|f| f == "package.json")
+                {
+                    options.append(&mut get_npm_tasks(entry.path()));
+                }
                 if let Some(ft) = entry.file_type() {
                     if ft.is_dir() {
                         continue;
@@ -35,8 +44,7 @@ impl<'de> ZellijWorker<'de> for TaskDiscoveryWorker {
                     // Check first line of file for shebang
                     let mut lines = BufReader::new(file).lines();
                     if lines.next().unwrap().unwrap().starts_with("#!") {
-                        eprintln!("found shebang: {:?}", entry.path());
-                        files.push(TaskOption {
+                        options.push(TaskOption {
                             task: entry.path().to_str().unwrap()[6..].to_string(),
                             runner: TaskRunner::Shell,
                         });
@@ -44,8 +52,8 @@ impl<'de> ZellijWorker<'de> for TaskDiscoveryWorker {
                 }
             }
         }
-        eprintln!("files: {:?}", files);
-        post_message_to_plugin("init".to_string(), serde_json::to_string(&files).unwrap());
+        eprintln!("files: {:?}", options);
+        post_message_to_plugin("init".to_string(), serde_json::to_string(&options).unwrap());
     }
 }
 
@@ -57,4 +65,25 @@ impl Display for TaskOption {
             TaskRunner::Shell => write!(f, "{}", self.task),
         }
     }
+}
+
+#[derive(Deserialize)]
+struct PackageJson {
+    scripts: HashMap<String, String>,
+}
+
+fn get_npm_tasks(path: &Path) -> Vec<TaskOption> {
+    let mut options: Vec<TaskOption> = vec![];
+    if let Ok(contents) = fs::read_to_string(path) {
+        // If there are no scripts, parsing will fail
+        if let Ok(json) = serde_json::from_str::<PackageJson>(&contents) {
+            for task in json.scripts.keys() {
+                options.push(TaskOption {
+                    task: task.to_string(),
+                    runner: TaskRunner::Npm,
+                });
+            }
+        }
+    }
+    options
 }
